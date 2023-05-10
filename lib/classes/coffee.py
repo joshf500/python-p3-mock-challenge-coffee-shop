c0abe9978be0230863a6c2ce672ddccbf91d1ff0
Josh Feldman <joshfeldman500@gmail.com>
2023-05-10 15:08:47 -0400

    coffee mock challenge done

--- lib/classes/coffee.py
+++ lib/classes/coffee.py
@@ -1,17 +1,41 @@
 class Coffee:
     def __init__(self, name):
         self.name = name
-        
+        self._orders=[]
+        self._customers=[]
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,name):
+        if type(name) == str and not hasattr (self, "name"):
+            self._name = name
+        else:
+            raise Exception ("Name already exists")
+
+
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
+        
+        if new_order and isinstance(new_order,Order):
+            self._orders.append(new_order)
+        return self._orders
     
     def customers(self, new_customer=None):
         from classes.customer import Customer
-        pass
+        
+        if new_customer not in self._customers and isinstance(new_customer,Customer):
+            self._customers.append(new_customer)
+        return self._customers
     
     def num_orders(self):
-        pass
+        return len(self._orders)
     
     def average_price(self):
-        pass
+        total=0
+        for i in self._orders:
+            total=total+i.price
+        avr=total/len(self._orders)
+        return avr
--- lib/classes/customer.py
+++ lib/classes/customer.py
@@ -1,11 +1,34 @@
 class Customer:
     def __init__(self, name):
         self.name = name
-        
+        self._orders=[]
+        self._coffees=[]
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self,name):
+        if isinstance (name,str) and 1 <= len(name) <= 15:
+            self._name = name
+        else:
+            raise Exception
+
     def orders(self, new_order=None):
         from classes.order import Order
-        pass
-    
+        if new_order and isinstance(new_order,Order):
+            self._orders.append(new_order)
+        return self._orders
+
     def coffees(self, new_coffee=None):
         from classes.coffee import Coffee
-        pass
+        if new_coffee not in self._coffees and isinstance(new_coffee,Coffee):
+            self._coffees.append(new_coffee)
+        return self._coffees
+
+    def create_order(coffee, price):
+        if isinstance(coffee, Coffee) and isinstance(price,int):
+            return Order(self,coffee,price)
+        else:
+            raise Exception("Inputs must be Coffee and integer types")
+    
